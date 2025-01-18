2b1941c19813265d22d710c448e6da6ff581c5c7
smik91 <125355749+smik91@users.noreply.github.com>
2025-01-18 22:57:54 +0300

    Adding migrations

---
+++ Money.Data/Migrations/20250118195722_ExpenseTransaction.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Money.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ExpenseTransaction : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ExpenseTransactions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    TransactionDate = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Amount = table.Column<decimal>(type: "numeric", nullable: false),
+                    MoneyAccountId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ExpenseTypeId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ExpenseTransactions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ExpenseTransactions_ExpenseTypes_ExpenseTypeId",
+                        column: x => x.ExpenseTypeId,
+                        principalTable: "ExpenseTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                    table.ForeignKey(
+                        name: "FK_ExpenseTransactions_MoneyAccounts_MoneyAccountId",
+                        column: x => x.MoneyAccountId,
+                        principalTable: "MoneyAccounts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExpenseTransactions_ExpenseTypeId",
+                table: "ExpenseTransactions",
+                column: "ExpenseTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExpenseTransactions_MoneyAccountId",
+                table: "ExpenseTransactions",
+                column: "MoneyAccountId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ExpenseTransactions");
+        }
+    }
+}
